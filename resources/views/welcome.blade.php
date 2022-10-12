db19ea03accbda744d4cd7c3ef87342121a1163e
naingminkhant <tharkhant777@gmail.com>
2022-10-12 11:31:00 +0630

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>Register</x-slot:title>
+    <div class=" bg-primary p-5">I am back!!</div>
+    <div class="bg-secondary p-5 text-white">I am back!!</div>
+    <div class="bg-dark p-5 text-white">I am back!!</div>
+</x-layout>
